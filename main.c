119d2f1b858ba6c4bd69594b200415e087b428f5
Lukas GIBONE <lugibone@e3r10p9.42.fr>
2019-12-03 18:21:16 +0100

    ok boomer

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: lugibone <lugibone@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/11/05 19:24:42 by lugibone          #+#    #+#             */
-/*   Updated: 2019/12/03 17:58:13 by lugibone         ###   ########.fr       */
+/*   Updated: 2019/12/03 18:20:06 by lugibone         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -50,17 +50,17 @@ int	md_set(t_scene *scene)
 		for (int y = 0; y < HEIGHT; y++)
 		{
 			i = 0;
-			c.r = x / zoomx + scene->plan.x1;
-			c.i = y / zoomy + scene->plan.y1;
 			z.r = 0;
 			z.i = 0;
+			c.r = x / zoomx + scene->plan.x1;
+			c.i = y / zoomy + scene->plan.y1;
 			i = md_loop(&c, &z, i);
 			while ((z.r * z.r) + (z.i * z.i) < 4 && i < scene->iteration)
 			i = md_loop(&c, &z, i);
 			if (i == scene->iteration)
 				fill_pixel(scene->str, x, y, scene->bg_color);
 			else
-				fill_pixel(scene->str, x, y, i * 0x010201);// / scene->iteration);
+				fill_pixel(scene->str, x, y, i * 0x010203 );/// scene->iteration);
 		}
 	}
 	return (1);
@@ -85,6 +85,22 @@ double	y_real(t_scene *scene, int y)
 	tmp = scene->plan.y2 - scene->plan.y1;
 	return (tmp2 * (scene->plan.y2 - scene->plan.y1) + scene->plan.y1);
 }
+void	zoom_out(t_scene *scene, int x, int y)
+{
+		scene->zoom *= 2;
+		scene->plan.x1 = x_real(scene, x) - scene->zoom;
+		scene->plan.x2 = x_real(scene, x) + scene->zoom;
+		scene->plan.y1 = y_real(scene, y) - scene->zoom;
+		scene->plan.y2 = y_real(scene, y) + scene->zoom;
+}
+void	zoom_in(t_scene *scene, int x, int y)
+{
+		scene->zoom /= 2;
+		scene->plan.x1 = x_real(scene, x) - scene->zoom;
+		scene->plan.x2 = x_real(scene, x) + scene->zoom;
+		scene->plan.y1 = y_real(scene, y) - scene->zoom;
+		scene->plan.y2 = y_real(scene, y) + scene->zoom;
+}
 
 int	deal_mouse(int key, int x, int y, t_scene *scene)
 {
@@ -92,13 +108,7 @@ int	deal_mouse(int key, int x, int y, t_scene *scene)
 	ft_putchar('\n');
 	
 	if (key == 5)
-	{
-		scene->zoom *= 2;
-		scene->plan.x1 = x_real(scene, x) - scene->zoom;
-		scene->plan.x2 = x_real(scene, x) + scene->zoom;
-		scene->plan.y1 = y_real(scene, y) - scene->zoom;
-		scene->plan.y2 = y_real(scene, y) + scene->zoom;
-	}
+		zoom_out(scene, x, y);
 	if (key == 3)
 	{
 		set_md(scene);
@@ -109,13 +119,7 @@ int	deal_mouse(int key, int x, int y, t_scene *scene)
 	if (key == 2 && scene->iteration > 1)
 		scene->iteration -= 10;
 	if (key == 4 && x > 0 && y > 0)
-	{
-		scene->zoom /= 2;
-		scene->plan.x1 = x_real(scene, x) - scene->zoom;
-		scene->plan.x2 = x_real(scene, x) + scene->zoom;
-		scene->plan.y1 = y_real(scene, y) - scene->zoom;
-		scene->plan.y2 = y_real(scene, y) + scene->zoom;
-	}
+		zoom_in(scene, x, y);
 	md_set(scene);
 	mlx_put_image_to_window(scene->mlx_ptr,
 			scene->win_ptr, scene->img_ptr, 0, 0);
